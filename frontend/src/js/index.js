db77343f6f6de72145db5e90067858177b5927a4
Frode Sundby <frodesun@gmail.com>
2018-08-09 13:48:13 +0200

    basic grid layout and responsive components in place

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -4,15 +4,20 @@ import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import { createBrowserHistory } from 'history'
+const bastaLogo = require('../img/basta.png')
 class App extends React.Component {
   render() {
     return (
-        <div className="wrapper">
-            <div className="topNav">topNav</div>
-            <div className="sideNav">sideNav</div>
-            <div className="main">main</div>
-            <div className="footer">footer</div>
-    </div>);
+        <wrapper>
+            <header>
+                <div className={"navBrand"}><span><img className="navLogo" src={bastaLogo} />basta</span></div>
+            </header>
+            <navigation>navigation</navigation>
+            <left></left>
+            <main>main content</main>
+            <right></right>
+            <footer></footer>
+    </wrapper>);
   }
 }
 
