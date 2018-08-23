b7cced79087ca1337c5aef576d8521853642e1fa
Castigere <even.haasted@nav.no>
2018-08-23 14:44:36 +0200

    History populated

--- frontend/src/js/containers/App.js
+++ frontend/src/js/containers/App.js
@@ -8,58 +8,59 @@ import { withRouter } from 'react-router-dom'
 import { userSessionRequest } from '../common/actionCreators'
 import { closeNavMenu, toggleNavMenu } from './navMenu/actionCreators'
 import NavMenu from './navMenu/NavMenu'
-
+import moment from 'moment'
+import localization from 'moment/locale/nb'
 
 const bastaLogo = require('../../img/basta.png')
 
-
 class App extends Component {
+  navigate(location) {
+    if (location === 'History') location = '/'
+    history.push(location.toLowerCase())
+  }
 
-    navigate(location) {
-        if (location === 'History') location = '/'
-        history.push(location.toLowerCase())
-    }
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch(userSessionRequest())
+    moment.locale('nb')
+  }
 
-    componentDidMount() {
-        const { dispatch } = this.props
-        dispatch(userSessionRequest())
-    }
-
-    render() {
-        return (
-            <div className='wrapper'>
-                <header>
-                    <div className='navBrand' href='/'>
-                        <img className='navLogo' src={bastaLogo} onClick={() => history.push('/')} />
-                        <div className='navName'>basta</div>
-                    </div>
-                </header>
-                <div className='navLeft' />
-                <nav>
-                    <NavMenu className='navTabs' />
-                    <div className='navButton' onClick={() => this.props.dispatch(toggleNavMenu())}>
-                        <i className='fa fa-bars fa-2x navSmallButton' />
-                    </div>
-                </nav>
-                <div className='navRight' />
-                <main onClick={() => this.props.dispatch(closeNavMenu())}>
-                    <NavMenuSmall />
-                    <Routes />
-                </main>
-                <footer></footer>
-            </div>)
-    }
+  render() {
+    return (
+      <div className="wrapper">
+        <header>
+          <div className="navBrand" href="/">
+            <img className="navLogo" src={bastaLogo} onClick={() => history.push('/')} />
+            <div className="navName">basta</div>
+          </div>
+        </header>
+        <div className="navLeft" />
+        <nav>
+          <NavMenu className="navTabs" />
+          <div className="navButton" onClick={() => this.props.dispatch(toggleNavMenu())}>
+            <i className="fa fa-bars fa-2x navSmallButton" />
+          </div>
+        </nav>
+        <div className="navRight" />
+        <main onClick={() => this.props.dispatch(closeNavMenu())}>
+          <NavMenuSmall />
+          <Routes />
+        </main>
+        <footer />
+      </div>
+    )
+  }
 }
 
 App.propTypes = {
-    dispatch: PropTypes.func,
-    user: PropTypes.object
+  dispatch: PropTypes.func,
+  user: PropTypes.object
 }
 
-const mapStateToProps = (state) => {
-    return {
-        user: state.user
-    }
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
 }
 
 export default withRouter(connect(mapStateToProps)(App))
