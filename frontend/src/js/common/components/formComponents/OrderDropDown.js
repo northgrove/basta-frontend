716ba846a39744eaffdd5adf8fa29d6573a9e990
Frode Sundby <frodesun@gmail.com>
2018-08-27 13:53:51 +0200

    adding ButtonGroup and DropDown to formComponentsπ

--- frontend/src/js/common/components/formComponents/OrderDropDown.js
+++ frontend/src/js/common/components/formComponents/OrderDropDown.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-const OrderDropDown = (props) => {
-    const {label, options} = props
+import Select from 'react-select'
+
+export const OrderDropDown = (props) => {
+    const {label, description, value, alternatives, onChange} = props
+    console.log(value)
+
     return (
-        <div>
-            <div className='orderItemLabel'>{label}</div>
-            <div className='orderItemOptions'>
+        <div className='formComponentGrid'>
+            <div className='formComponentLabel'>{label}</div>
+            <div className='formComponentDropdownField'>
+                    <Select options={mapToOptions(alternatives)}
+                            setValue={value}
+                            onChange={(e) => onChange(e.value)}/>
+                <div className='formComponentDescription'>{description}</div>
             </div>
         </div>
+    )
+}
+const mapToOptions = (alternatives) => {
+    return alternatives.map((alt) => {
+            return {label: alt, value: alt}
+        }
     )
 }
 
 OrderDropDown.propTypes = {
     label: PropTypes.string,
-    options: PropTypes.array
+    description: PropTypes.string,
+    value: PropTypes.number,
+    alternatives: PropTypes.array,
+    onChange: PropTypes.func,
 }
 
 export default OrderDropDown
