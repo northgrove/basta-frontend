52ac78ea297909cc618c22d5fcdb7f57ad15bb93
Castigere <even.haasted@nav.no>
2018-08-28 16:24:36 +0200

    new logon box

--- frontend/src/js/containers/login/Login.js
+++ frontend/src/js/containers/login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react'
+const bastaLogo = require('../../../img/basta.png')
+const navLogo = require('../../../img/nav.png')
 
 const Login = props => {
   return (
-    <div className="login-container">
-      <a href="http://localhost:8080/login">Sign In with Azure AD</a>
+    <div className="login-background">
+      <div className="login-container">
+        <img className="login-basta-logo" src={bastaLogo} />
+        <div className="login-box">
+          <img className="login-nav-logo" src={navLogo} />
+          <div className="login-box-header">
+            Single sign-on to <strong> NAV </strong>
+          </div>
+          <div className="login-box-body">
+            <center> Authenticate by loggin into NAV's single sign-on provider. </center>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
