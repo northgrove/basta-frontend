f5988722d22247122d10ce3845ae84a2921150a9
Castigere <even.haasted@nav.no>
2018-08-29 15:08:55 +0200

    automatic logout after session expires

--- frontend/src/js/common/sagas/index.js
+++ frontend/src/js/common/sagas/index.js
@@ -4,6 +4,7 @@ import { watchUser } from './user'
 import { watchInitialize } from './initialize'
 import { watcHistory } from '../../containers/history/sagas'
 import { watchForm } from '../components/sagas'
+import { watchPollSession } from './polling'
 
 export default function*() {
   yield all([
@@ -11,6 +12,7 @@ export default function*() {
     call(watchUser),
     call(watcHistory),
     call(watchForm),
-    call(watchInitialize)
+    call(watchInitialize),
+    call(watchPollSession)
   ])
 }
