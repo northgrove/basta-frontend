dad6ce95abe774bcfdf67c7ef73d3696f8c666a5
Frode Sundby <frodesun@gmail.com>
2018-08-29 16:56:41 +0200

    litt info til Kjetil i getRoles

--- api/config/getroles.js
+++ api/config/getroles.js
@@ -13,10 +13,10 @@ exports.matchRoles = ({groups}) => {
 }
 
 const roles = {
-    ROLE_SUPERUSER: ['c91b58c6-c7e7-4160-9b58-5187abb0bb6b'],
-    ROLE_PROD_OPERATIONS: ['9fad3009-d179-4a8f-b73d-5f1e28dc6013'],
-    ROLE_OPERATIONS: ['0'],
-    ROLE_USER: ['0'],
+    ROLE_SUPERUSER: ['c91b58c6-c7e7-4160-9b58-5187abb0bb6b'], //0000-GA-BASTA_SUPERUSER
+    ROLE_PROD_OPERATIONS: ['9fad3009-d179-4a8f-b73d-5f1e28dc6013'], //0000-GA-env-config-TestAdmin, 0000-GA-BASTA_SUPERUSER
+    ROLE_OPERATIONS: ['0'], //0000-GA-env-config-TestAdmin, 0000-GA-BASTA_SUPERUSER
+    ROLE_USER: ['0'], //All ad users
     TEST1: ['xxx', 'yyy'],
     TEST2: ['yyy']
 }
