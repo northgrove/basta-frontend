87c53426d268f321b0bb5ee1635f1877cafffa4b
Castigere <even.haasted@nav.no>
2018-09-05 11:27:29 +0200

    error handing for image request

--- api/src/controllers/msgraph.js
+++ api/src/controllers/msgraph.js
@@ -4,15 +4,23 @@ const token = require('./getAccesstoken')
 exports.getUserPhoto = async ({ userUpn }) => {
   let userPhoto = ''
   const aToken = await token.getAccessToken()
-
-  return request
-    .get({
-      headers: { 'content-type': 'image/jpg' },
-      url: `https://graph.microsoft.com/beta/users/${userUpn}/photo/$value`,
-      auth: { bearer: aToken }
-    })
-    .then(response => {
-      userPhoto = 'data:image/jpg;base64,' + new Buffer(response).toString('base64')
-      return userPhoto
-    })
+  try {
+    return request
+      .get({
+        headers: { 'content-type': 'image/jpg' },
+        url: `https://graph.microsoft.com/beta/users/${userUpn}/photo/$value`,
+        auth: { bearer: aToken }
+      })
+      .then(response => {
+        console.log(response.status)
+        userPhoto = 'data:image/jpg;base64,' + new Buffer(response).toString('base64')
+        return userPhoto
+      })
+      .catch(err => {
+        console.log('No picture found for user')
+        return null
+      })
+  } catch (err) {
+    throw err
+  }
 }
