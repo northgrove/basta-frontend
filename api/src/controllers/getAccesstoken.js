e84d237e335c508fa6a38f70c286ce00ecf3e4be
Kjetil Nordlund <kjetil@jll.no>
2018-09-06 13:00:25 +0200

    getaccesstoken opprydding

--- api/src/controllers/getAccesstoken.js
+++ api/src/controllers/getAccesstoken.js
@@ -5,18 +5,6 @@ let ms_access_token = ''
 exports.getAccessToken = async tokenURI => {
   let parameters = ''
   try {
-    // Setter parametere for Login URL mot graph.microsoft.com
-    /*
-    if (tokenURI.match(/localhost/)) {
-      parameters = {
-        client_id: 'test_id',
-        resource: 'testResource',
-        redirect_uri: 'testRedirectURL',
-        grant_type: 'Test_client_credentials',
-        client_secret: 'testSecret'
-      }
-    } else {
-      */
     parameters = {
       client_id: process.env['BASTAAZURECONFIG_CLIENTID'],
       resource: config.resourceURL,
@@ -24,11 +12,6 @@ exports.getAccessToken = async tokenURI => {
       grant_type: 'client_credentials',
       client_secret: process.env['BASTAAZURECONFIG_CLIENTSECRET']
     }
-
-    // }
-
-    // const parameters2 = `client_id=${config.clientID}&scope=${config.resourceURL}&code=${code}&redirect_uri=http://localhost:8080/&grant_type=authorization_code&client_secret=${config.clientSecret}`
-    // console.log(parameters)
     await request.post({ url: tokenURI, formData: parameters }, function callback(
       err,
       httpResponse,
