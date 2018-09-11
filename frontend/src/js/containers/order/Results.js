9fa3ed084e9285ac15c356f056d50f826ac4b100
Frode Sundby <frodesun@gmail.com>
2018-09-11 09:55:28 +0200

    Results-view for VM på plass.

--- frontend/src/js/containers/order/Results.js
+++ frontend/src/js/containers/order/Results.js
@@ -2,40 +2,53 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import ReactTooltip from 'react-tooltip'
+import Select from 'react-select'
+import moment from 'moment'
+import history from '../../common/history'
 
 function buildResultBody(data, type) {
   switch (type) {
     case 'VM':
       return (
         <div>
-          <table style={{ width: '100%' }}>
-            <thead>
-              <tr>
-                <td>host</td>
-                <td>history</td>
-                <td>Operations</td>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map(result => {
-                return (
-                  <tr key={result.resultName}>
-                    <td>{result.resultName}</td>
-                    <td>{result.history.toString()}</td>
-                    <td>Operations</td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
+          {data.map(result => {
+            const options = mapToOptions(result.history)
+            return (
+              <div className="resultLine" key={'resultline_' + result.id}>
+                <div className="result">{result.resultName}</div>
+                <div className="history">
+                  <Select
+                    isSearchable={false}
+                    onChange={e => history.push(`/orders/${e.value}`)}
+                    options={options}
+                    value={options[0]}
+                  />
+                </div>
+                <div className="operations">
+                  <i className="fa fa-wrench" />{' '}
+                </div>
+              </div>
+            )
+          })}
         </div>
       )
   }
 }
 
+const mapToOptions = results => {
+  return results.map(result => {
+    const label =
+      result.id +
+      ' ' +
+      result.orderOperation.toLowerCase() +
+      ' (' +
+      moment(result.created).fromNow() +
+      ')'
+    return { label, value: result.id }
+  })
+}
 const Results = props => {
   const { data, type } = props
-  console.log(data)
   return (
     <div className="results">
       <div className="panel panel-default">
