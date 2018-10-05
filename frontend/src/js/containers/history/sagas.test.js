896e1a0b84c973ae566bc0c8b3e51b11ba6d152c
Castigere <even.haasted@nav.no>
2018-10-05 10:05:32 +0200

    assert based on local timezone, added dynamic time resolve in test

--- frontend/src/js/containers/history/sagas.test.js
+++ frontend/src/js/containers/history/sagas.test.js
@@ -7,6 +7,7 @@ import { getOrders } from './selectors'
 import * as matchers from 'redux-saga-test-plan/matchers'
 import { throwError } from 'redux-saga-test-plan/providers'
 import { getUrl } from '../../common/utils'
+import moment from 'moment'
 
 it('(History view sagas - applyOrderHistoryFilter) dispatches with filter, selects all orders from history, formats them, and returns order matching filter', () => {
   const action = { filter: 'JBOSS' }
@@ -19,9 +20,9 @@ it('(History view sagas - applyOrderHistoryFilter) dispatches with filter, selec
       filteredOrderHistory: [
         {
           id: 24402,
-          created: 'Aug 15, 2018 8:37 AM',
+          created: moment(1534315029884).format('lll'),
           createdBy: 'srvfasit',
-          updated: 'Aug 15, 2018 8:37 AM',
+          updated: moment(1534315061586).format('lll'),
           updatedBy: 'srvOrchestrator',
           updatedByDisplayName: 'Service User',
           createdByDisplayName: 'Service User',
