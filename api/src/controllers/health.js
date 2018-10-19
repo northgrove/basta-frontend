9a4ca30f3734677ddc9de0ad906905e44440c935
matsbyfl <mats@byfuglien.net>
2018-10-19 14:41:27 +0200

    debug logging

--- api/src/controllers/health.js
+++ api/src/controllers/health.js
@@ -1,7 +1,10 @@
+const logger = require('morgan')
+
 exports.isAlive = () => {
   console.log('calling isalive')
 
   return (req, res) => {
+    console.log('calling isalive before 200')
     res.status(200).end()
   }
 }
