4e3c2c4aecb033abf1276b98d62867282f51e0dd
Frode Sundby <frodesun@gmail.com>
2018-10-23 13:19:18 +0200

    Fix user menu

--- frontend/src/js/containers/navMenu/NavUserMenu.js
+++ frontend/src/js/containers/navMenu/NavUserMenu.js
@@ -1,40 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import connect from 'react-redux/es/connect/connect'
 
-const navUserMenu = props => {
-  const { user, userLogout, dispatch } = props
-
-  let roles
-  if (user.isUserAuthenticated) {
-    roles = user.userProfile.roles.join(', ')
-  } else {
-    roles = 'error'
+export class NavUserMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayMenu: false
+    }
   }
-  return (
-    <div className="navUserMenu">
-      <div className="navUserImage">
-        <img src={user.userProfile.photo} />
-      </div>
-      <div className="navUserMenu-content">
-        <a href="https://account.activedirectory.windowsazure.com/r#/profile">
-          {user.userProfile.userName}
-        </a>
-        <hr />
-        <p>
-          {' '}
-          Access: <br /> {roles}{' '}
-        </p>
-        <hr />
-        <a
-          className="navUserMenu-signout"
-          onClick={() => {
-            dispatch(userLogout())
-          }}
-        >
-          <i className="fa fa-sign-out" /> Sign out
-        </a>
-      </div>
-    </div>
-  )
-}
 
-export default navUserMenu
+  render() {
+    const { user, userLogout, dispatch } = this.props
+    const { displayMenu } = this.state
+
+    let roles
+    if (user.isUserAuthenticated) {
+      roles = user.userProfile.roles.join(', ')
+    } else {
+      roles = 'error'
+    }
+    return (
+      <div className="navUserMenu">
+        <div className="navUserImage" onClick={() => this.setState({ displayMenu: !displayMenu })}>
+          <img src={user.userProfile.photo} />
+        </div>
+        {displayMenu ? (
+          <div className="navUserMenu-content">
+            <a href="https://account.activedirectory.windowsazure.com/r#/profile">
+              {user.userProfile.userName}
+            </a>
+            <hr />
+            <p>
+              {' '}
+              Access: <br /> {roles}{' '}
+            </p>
+            <hr />
+            <a
+              className="navUserMenu-signout"
+              onClick={() => {
+                dispatch(userLogout())
+              }}
+            >
+              <i className="fa fa-sign-out" /> Sign out
+            </a>
+          </div>
+        ) : null}
+      </div>
+    )
+  }
+}
+NavUserMenu.propTypes = {}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+export default connect(mapStateToProps)(NavUserMenu)
