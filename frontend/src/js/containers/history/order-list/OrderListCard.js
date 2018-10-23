5b0e6f16eb2b8c82b64b743ebcc036a50cf9bd71
Frode Sundby <frodesun@gmail.com>
2018-10-23 12:36:41 +0200

    adds correct image for different ordertypes

--- frontend/src/js/containers/history/order-list/OrderListCard.js
+++ frontend/src/js/containers/history/order-list/OrderListCard.js
@@ -4,11 +4,72 @@ import moment from 'moment'
 import { Link } from 'react-router-dom'
 import OrderStatusBadge from '../../../common/components/formComponents/OrderStatusBadge'
 
-const image = require('../../../../img/orderTypes/websphere.png')
+const wasImage = require('../../../../img/orderTypes/websphere.png')
+const jbossImage = require('../../../../img/orderTypes/jboss.png')
+const wildflyImage = require('../../../../img/orderTypes/wildfly.png')
+const libertyImage = require('../../../../img/orderTypes/liberty.png')
+const securityImage = require('../../../../img/orderTypes/security.png')
+const openamImage = require('../../../../img/orderTypes/openam.png')
+const redhatImage = require('../../../../img/orderTypes/redhat.png')
+const containerlinuxImage = require('../../../../img/orderTypes/containerlinux.png')
+const dbImage = require('../../../../img/orderTypes/oracle.png')
+const windowsImage = require('../../../../img/orderTypes/windows.png')
+const bigipImage = require('../../../../img/orderTypes/big-ip.png')
+const mqImage = require('../../../../img/orderTypes/mq.png')
+const developertoolsImage = require('../../../../img/orderTypes/devtools.png')
+const iappImage = require('../../../../img/orderTypes/devtools-iapp.png')
+const coreosImage = require('../../../../img/orderTypes/containerlinux.png')
+const unknown = require('../../../../img/orderTypes/unknown.png')
+
+function imageType(orderDescription) {
+  console.log(orderDescription)
+  switch (orderDescription.toLowerCase()) {
+    case 'was deployment manager':
+      return wasImage
+    case 'was nodes':
+      return wasImage
+    case 'was9 nodes':
+      return wasImage
+    case 'big-ip':
+      return bigipImage
+    case 'wildfly':
+      return wildflyImage
+    case 'jboss':
+      return jbossImage
+    case 'oracle':
+      return dbImage
+    case 'plain linux':
+      return redhatImage
+    case 'certificate':
+      return securityImage
+    case 'dev tools':
+      return developertoolsImage
+    case 'dockerhost':
+      return developertoolsImage
+    case 'windows internet server':
+      return windowsImage
+    case 'windows applicationserver':
+      return windowsImage
+    case 'topic':
+      return mqImage
+    case 'queue':
+      return mqImage
+    case 'channel':
+      return mqImage
+    case 'credential':
+      return securityImage
+    case 'lightweight linux':
+      return containerlinuxImage
+    default:
+      return unknown
+  }
+  return wasImage
+}
 
 const OrderCard = props => {
   const { order } = props
   if (!order) return null
+  const image = imageType(order.orderDescription)
   return (
     <Link to={`/orders/${order.id}`}>
       <div className="orderListCard">
