691c6c6e631ba83b7c9a8879cd8d035eee354775
matsbyfl <mats@byfuglien.net>
2018-10-30 14:46:28 +0100

    removed unused routes

--- api/test/routes/index.js
+++ api/test/routes/index.js
@@ -17,16 +17,4 @@ router.get(`${api}/user/profile`, user.getUserProfile())
 
 router.get(`${api}/user/session`, user.userSessionLookup())
 
-// ORDERS
-
-//router.get(`${api}/orders/page/:pageId/:pageSize/:toDate/:fromDate`, order.getOrders())
-
-//router.get(`${api}/orders/:id/`, order.getOrder())
-
-//router.post(`${api}/orders/:type/`, order.postOrder())
-
-// STATUSLOG
-
-//router.get(`${api}/orders/:id/statuslog`, order.getStatusLog())
-
 module.exports = router
